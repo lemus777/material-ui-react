d3cf50ea8ddca618c16ba4b09abdc8a79b34b93d
Lemus <lemus777@gmail.com>
2023-04-04 19:59:53 +0200

    searcher

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Container } from '@mui/material';
+import Searcher from './components/Searcher';
+import { getGitHubUser } from './services/users';
 
 const App = () => {
+
+    const [inputUser, setInputUser] = useState('octocat');
+    const [userStater, setUserStater] = useState('inputUser');
+
+    const gettingUser = async (user) => {
+        const userResponse = await getGitHubUser(user);
+        console.log(userResponse);
+    }
+
+    useEffect(() => {
+        gettingUser(inputUser);
+    }, [])
+
     return(
-        <div>Soy un platzinauta</div>
+        <Container sx={{
+            background: 'whitesmoke',
+            width: '80vw',
+            height: '500px',
+            borderRadius: '16px',
+            marginTop: '40px',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center'
+        }}>
+            <Searcher inputUser={inputUser} setInputUser={setInputUser} />
+        </Container>
     )
 };
 
